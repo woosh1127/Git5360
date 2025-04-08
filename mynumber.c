c094871cbaab7ef5800cb5e561845626f14c11da
woosh1127 <woosh1127@gmail.com>
2025-04-08 20:21:44 +0900

    even number

--- mynumber.c
+++ mynumber.c
@@ -3,6 +3,12 @@
 int main() {
 	for(int i =0, i <= 100, i++) {
 		if(i % 2 != 0) {
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	for (int i = 0, i <= 100, i++) {
+		if(i % 2 == 0) {
 			printf("%d", i)
 		}
 	}
